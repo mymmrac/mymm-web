1dfae29e36664de61727fdc0c516c4a28f24ecab
Artem Yadelskyi <mymmrac@gmail.com>
2021-12-22 23:52:11 +0200

    Basic server setup

--- server/main.go
+++ server/main.go
@@ -1,22 +1,77 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"math/rand"
 	"net/http"
 	"os"
+	"os/signal"
+	"runtime"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!\n\n%d\n\nPath: %q\n", rand.Int(), r.URL)
-	})
-
-	fmt.Println("Starting server...")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+func cpuHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
 
+const addr = "127.0.0.1:8080"
+
+const shutdownTimeout = 10 * time.Second
+
+func main() {
+	log := logrus.New()
+	log.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp:   true,
+		TimestampFormat: time.RFC1123,
+		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
+			return frame.Function, fmt.Sprintf(" %s:%d", frame.File, frame.Line)
+		},
+	})
+	log.SetReportCaller(true)
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "%d\n", rand.Int())
+		if err != nil {
+			log.Errorf("Root: %v", err)
+		}
+	})
+
+	sys := r.PathPrefix("/system").Subrouter()
+	sys.HandleFunc("/cpu", cpuHandler)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         addr,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+		IdleTimeout:  30 * time.Second,
+	}
+
+	go func() {
+		log.Infof("Starting server at: http://%s", addr)
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Errorf("Serve: %v", err)
+		}
+	}()
+
+	exitSignal := make(chan os.Signal, 1)
+	signal.Notify(exitSignal, os.Interrupt)
+
+	<-exitSignal
+
+	log.Info("Shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Errorf("Shutdown: %v", err)
+	}
+}
