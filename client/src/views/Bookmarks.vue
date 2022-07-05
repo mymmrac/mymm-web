91386029392079c75a7434ceae6cbd7438cd9d42
Artem Yadelskyi <mymmrac@gmail.com>
2022-07-05 12:00:25 +0300

    Updated font

--- client/src/views/Bookmarks.vue
+++ client/src/views/Bookmarks.vue
@@ -7,8 +7,27 @@
                     <i class="bi bi-journal-bookmark text-3xl"></i>
                     <p class="uppercase">Bookmarks</p>
                 </div>
-                <i class="bi bi-bookmark-plus text-3xl self-end cursor-pointer m-hover-scale m-hover-highlight"></i>
+                <i class="bi bi-bookmark-plus text-3xl self-end m-hover-scale m-hover-highlight"></i>
             </div>
+        </div>
+
+        <div class="m-box flex items-center justify-between gap-3">
+            <i class="bi bi-search text-3xl"></i>
+            <input type="text" placeholder="Search..."
+                   class="flex-1 border-0 border-b focus:ring-0 focus:border-green-400">
+            <i class="bi bi-backspace text-3xl m-hover-scale m-hover-highlight"></i>
+        </div>
+
+        <div class="flex gap-2">
+            <div class="m-box flex items-center gap-2 cursor-pointer m-hover-scale group">
+                <i class="bi bi-grid text-2xl text-green-400 m-hover-highlight-group"></i>
+                <p>All</p>
+            </div>
+            <div class="m-box flex items-center gap-2 cursor-pointer m-hover-scale group">
+                <i class="bi bi-code-slash text-2xl m-hover-highlight-group"></i>
+                <p>Dev</p>
+            </div>
+
         </div>
 
         <div class="m-box">
