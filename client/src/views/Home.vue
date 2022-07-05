b11f092eaa5d13bc0772d6859d43500ec333e351
Artem Yadelskyi <mymmrac@gmail.com>
2022-07-05 12:45:23 +0300

    Updated home view

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,21 +1,42 @@
 <template>
-    <div class="flex flex-wrap gap-4">
-        <router-link to="/bookmarks" tag="div" class="m-box m-item m-hover-scale">
-            <i class="bi bi-journal-bookmark"></i>
-            <p>Bookmarks</p>
-        </router-link>
-        <router-link to="/system" tag="div" class="m-box m-item m-hover-scale">
-            <i class="bi bi-cpu"></i>
-            <p>System</p>
-        </router-link>
+    <div>
+        <div class="grid m-grid gap-2">
+            <router-link v-for="item in menu" :to="item.link" tag="div" class="m-box m-item m-hover-scale">
+                <i class="bi" :class="`bi-${item.icon}`"></i>
+                <p>{{ item.name }}</p>
+            </router-link>
+        </div>
     </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+type MenuButton = {
+    name: string,
+    link: string,
+    icon: string,
+}
+
+const menu: MenuButton[] = [
+    {
+        name: "Bookmarks",
+        link: "/bookmarks",
+        icon: "journal-bookmark",
+    },
+    {
+        name: "System",
+        link: "/system",
+        icon: "cpu",
+    },
+]
+</script>
 
 <style lang="scss" scoped>
+.m-grid {
+    grid-template-columns: repeat(auto-fill, minmax(192px, 1fr));
+}
+
 .m-item {
-    @apply w-[192px] h-[192px] flex flex-col justify-center items-center uppercase;
+    @apply aspect-square flex flex-col justify-center items-center uppercase;
 
     i {
         @apply text-8xl mb-4;
